3805f5fdb2f7c792a2f6f4cfa5bf7c0ec7c600e6
Bendi7080 <s21samira05@gmail.com>
2026-01-12 17:52:26 +0100

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener("install",e=>{
-e.waitUntil(
-caches.open("pnake").then(c=>c.addAll(["./","pnake.html","manifest.json"]))
-);
-});
-
-self.addEventListener("fetch",e=>{
-e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
